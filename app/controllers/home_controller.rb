898c0575034d3bd26cdbb19e1616d669aff6eea1
muratrl <murat@rentlever.com>
2015-12-27 12:12:50 -0800

    added form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def raffle
+    @client=Client.new
   end
 
   def raffleEnter
+    @client = Client.new(client_params)
+    @client.save
+
+  end
+  def client_params
+   params.require(:client).permit(:name, :email)
   end
 end
